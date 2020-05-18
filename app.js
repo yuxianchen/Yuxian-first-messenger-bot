f25cb7e526abf9182dd206bb8df1228d899a3e06
Yuxian Chen <yuxianchen@fb.com>
2020-05-18 13:26:49 +0100

    ad log

--- app.js
+++ app.js
@@ -82,7 +82,7 @@ app.get('/webhook', function(req, res) {
  */
 app.post('/webhook', function (req, res) {
   var data = req.body;
-
+  console.log(`let me see request payload: ${JSON.stringify(req)}`)
   // Make sure this is a page subscription
   if (data.object == 'page') {
     // Iterate over each entry
